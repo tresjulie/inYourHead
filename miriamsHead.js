05ddb887b9395f402fcca9e69280717256bd436e
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-06-21 11:53:50 +0200

    Update miriamsHead.js

--- miriamsHead.js
+++ miriamsHead.js
@@ -1,7 +1,7 @@
 import SpeechBubble from "./SpeechBubble.js";
 import PersonalBubble from "./PersonalBubble.js";
 let speech = new SpeechBubble(
-  "Hallo Kisten und Julie :)))  Mein Name ist sehr lange Nachricht.  Ich wüsste zu gerne, ob ich funktioniere, oder nciht, aber noch ish poehf uhlajhfe lkdfsadfa d fasddsffffffffffffffffffffffffffffffffffffffff ffffffffffffffffffffffffffffffffffffffffffffffffffffff ffffffffffffffffffffffffffffff fffffffffff ffffffffffffffffff fffffffffffffffffffffffff fffffffffffff ff    ffffffffffffffffffffffffffffffffff fffffffffff f f  f  f jkhajktv iogakjeh rakjhr cljkhr akjehr akjhe  ",
+  "Hallo Kirrrrrrrrrrrrrrrrrsten ug ggccc hhcghnd Julie :)))  Mein Name ist sehr lange Nachricht.  Ich wüsste zu gerne, ob ich funktioniere, oder nciht, aber noch ish poehf uhlajhsalkj alksj lksaj lksj lkajs lkasj lkasj lkajs kjklllllllllllllajalllllllll  jslkdjldksjkl akj lksjdkj laskjfe lkdfsadfa d fasddsffffffffffffffffffffffffffffffffffffffff ffffffffffffffffffffffffffffffffffffffffffffffffffffff ffffffffffffffffffffffffffffff fffffffffff ffffffffffffffffff fffffffffffffffffffffffff fffffffffffff ff    ffffffffffffffffffffffffffffffffff fffffffffff f f  f  f jkhajktv iogakjeh rakjhr cljkhr akjehr akjhe  ",
   "person1"
 );
 let speechi = new SpeechBubble("hiiiiiiiii ", "person1");
@@ -10,9 +10,9 @@ let bPbubble = new PersonalBubble(0, "right");
 let otherbubble = new PersonalBubble(0, "left");
 let bubble = loadImage("speechp.png");
 function draw() {
-  background(0, 255, 0);
-  speech.all(speechi.hitterNumber, bPbubble.direction, speech.message);
+  clear();
+  speech.all(speechi.hitterNumber, bPbubble.direction);
   if (speech.hitterNumber >= 1) {
-    speechi.all(0, otherbubble.direction, speechi.message);
+    speechi.all(0, otherbubble.direction);
   }
 }
