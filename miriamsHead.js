2e4d3ab9e387c0ff9397a817dcc65aaf61dc3b1a
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-06-21 03:07:31 +0200

    Update miriamsHead.js

--- miriamsHead.js
+++ miriamsHead.js
@@ -1,30 +1,18 @@
 import SpeechBubble from "./SpeechBubble.js";
 import PersonalBubble from "./PersonalBubble.js";
 let speech = new SpeechBubble(
-  100,
-  200,
-  "Hallo Kirssvsvs sgten ",
-  100,
-  "person1",
-  false,
-  0
+  "Hallo Kisten und Julie :)))  Mein Name ist sehr lange Nachricht.  Ich wüsste zu gerne, ob ich funktioniere, oder nciht, aber noch ish poehf uhlajhfe lkdfsadfa d fasddsffffffffffffffffffffffffffffffffffffffff ffffffffffffffffffffffffffffffffffffffffffffffffffffff ffffffffffffffffffffffffffffff fffffffffff ffffffffffffffffff fffffffffffffffffffffffff fffffffffffff ff    ffffffffffffffffffffffffffffffffff fffffffffff f f  f  f jkhajktv iogakjeh rakjhr cljkhr akjehr akjhe  ",
+  "person1"
 );
-let speechi = new SpeechBubble(
-  100,
-  200,
-  "hiiiiiiiii ",
-  100,
-  "person1",
-  false,
-  0
-);
-let bPbubble = new PersonalBubble(0, "left");
-let otherbubble = new PersonalBubble(0, "right");
+let speechi = new SpeechBubble("hiiiiiiiii ", "person1");
+
+let bPbubble = new PersonalBubble(0, "right");
+let otherbubble = new PersonalBubble(0, "left");
 let bubble = loadImage("speechp.png");
 function draw() {
   background(0, 255, 0);
-  speech.all(speechi.hitterNumber, bPbubble.direction, bubble, speech.message);
+  speech.all(speechi.hitterNumber, bPbubble.direction, speech.message);
   if (speech.hitterNumber >= 1) {
-    speechi.all(0, otherbubble.direction, bubble, speechi.message);
+    speechi.all(0, otherbubble.direction, speechi.message);
   }
 }
