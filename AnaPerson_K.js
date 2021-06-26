07802956e26aac9af303e9e3a24b7a987ddf385a
KirstenU <73792785+KirstenU@users.noreply.github.com>
2021-06-26 13:18:20 +0200

    AnaPerson mit Einbindung in Hauptdatei

---
+++ AnaPerson_K.js
@@ -0,0 +1,53 @@
+let Namearray = ["Test"];
+let Hoverthoughtarray = ["erster Test", "zweiter Test", "dritter Test"];
+
+export default class AnaPerson {
+  constructor(x, y, width, height, name, currentMood, hoverthought) {
+    //müsste man nicht eine alternative finden zu width und height beim hovern weil das Bild von der bP ja keine viereckige umrandung hat?
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.hoverthought = hoverthought;
+    this.name = name;
+    this.currentMood = currentMood;
+  }
+  hover() {
+    //bei hover wird gedankenblase angezeigt
+    //bP ist erstmal ein rechteck
+    rect(this.x, this.y, this.width, this.height);
+    if (
+      mouseX >= this.x &&
+      mouseX <= this.x + this.width &&
+      mouseY >= this.y &&
+      mouseY <= this.y + this.height
+    ) {
+      push();
+      fill(255);
+      //wird Gedankenblase gecodet? uuund wenn ja, können es ja eig feste Werte wie hier sein oder? Ist ja immer die gleiche Position
+      ellipse(450, 100, 200, 100);
+      ellipse(370, 170, 40);
+      ellipse(340, 200, 20);
+      fill(0);
+      Hoverthoughtarray.push(ana.hoverthought);
+      text(Hoverthoughtarray[ana.hoverthought], 415, 100);
+      pop();
+    } else {
+      fill(100);
+    }
+  }
+  mood() {
+    push();
+    textSize(30);
+    //je nach mood wird ein anderes bild displayt
+    if (this.currentMood === "laugh") {
+      //hier dann anstatt halt ein bild)
+      text("😂", 110, 90);
+    } else if (this.currentMood === "smilecry") {
+      text("🥲", 110, 90);
+    } else if (this.currentMood === "heart") {
+      text("😍", 110, 90);
+    }
+    pop();
+  }
+}
--- KirstensHead.js
+++ KirstensHead.js
@@ -1,10 +1,22 @@
 import Button from "./Button.js";
-let name = "TEST";
+import AnaPerson from "./AnaPerson_K.js";
 
-let firstButton = new Button(100, 100, name);
-firstButton.display();
-//firstButton.colourchange();
+let name = "TEST";
+let firstButton = new Button(400, 300, name);
 
 function mouseClicked() {
   firstButton.hitTest();
+  //Farbe ändern bei Klick? Sonst ohne if
+  //firstButton.colourchange();
+}
+
+let ana = new AnaPerson(100, 100, 200, 300, "Miriam", "heart", "1");
+Namearray.push(ana.name);
+console.log(Namearray[1]);
+
+function draw() {
+  clear();
+  ana.hover();
+  ana.mood();
+  firstButton.display();
 }
