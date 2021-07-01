475002db2330099e0b6b6119a2491f4bcc5b9e3a
KirstenU <73792785+KirstenU@users.noreply.github.com>
2021-07-01 20:13:01 +0200

    definierte Bereiche für Buttons

---
+++ Bereiche_Buttons_etc.js
@@ -0,0 +1,29 @@
+//Dialoge; ohne Randabstand?
+rect(740, 340, 422, 360);
+
+//Leiste oben
+//Abstand immer 35, y immer 35, gleiche Höhe
+rect(35, 35, 50, 50, 5);
+rect(120, 35, 250, 50, 5);
+ellipse(435, 60, 50);
+
+//Instant Feedback
+rect(650, 35, 477, 145, 10);
+
+//ein Button mittig
+rect(465, 450, 230, 65, 10);
+
+//ein Button an Seite unten: Verständnis, FreundIn, Elternteil und Namensgebung
+rect(825, 560, 230, 65, 10);
+
+//Position wählen, zwei Buttons mittig
+rect(320, 330, 230, 65, 10);
+rect(610, 330, 230, 65, 10);
+
+//Startseite
+rect(290, 410, 230, 65, 10);
+rect(645, 410, 230, 65, 10);
+
+//Feedbacks, zwei Buttons an Seite (35er Abstand zueinander und zum Rand)
+rect(825, 560, 230, 65, 10);
+rect(560, 560, 230, 65, 10);
