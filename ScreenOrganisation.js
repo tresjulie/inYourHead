0a098a43b5b92b85b69b3d5ecb7adc5e3b4e50ad
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-07-02 10:31:51 +0200

    Update ScreenOrganisation.js

--- ScreenOrganisation.js
+++ ScreenOrganisation.js
@@ -57,6 +57,7 @@ export default class ScreenOrganisation {
       teller,
       wohnzimmer,
     ];
+    //wollen wir auch unabhängig von den Bildern versuchen, diese Zahlen zu benutzen, oder die states ausschreiben?
     this.pictruesPregame = [tryIt];
     if (this.state === "pregame") {
       image(tryIt, 0, 0, 100, 100);
@@ -64,7 +65,73 @@ export default class ScreenOrganisation {
     if (this.state === "ingame") {
       image(this.background[this.backgroundNumber], 0, 0, 100, 100);
     }
+    /*Alternative: (backgroundstate und pregamestate  und weitere werte für dei Arraysin constructor)
+if (this.backgroundstate=== abendessen){
+  image(abendessen,0,0,100,100);
+//Beispielwerte
+  bubbleMin=0;
+  bubbleMax=2;
+button=2;
 
+}
+      if (this.backgroundstate=== baeckerei){
+  image(baeckerei,0,0,100,100);
+}
+      if (this.backgroundstate=== bekannte){
+  image(bekannte,0,0,100,100);
+}
+     if (this.backgroundstate===  bekannteTuer){
+  image(bekannteTuer,0,0,100,100);
+}
+      if (this.backgroundstate=== blumenladen){
+  image(blumenladen,0,0,100,100);
+}
+      if (this.backgroundstate=== entensee){
+  image(entensee,0,0,100,100);
+}
+      if (this.backgroundstate=== haeuser){
+  image(haeuser,0,0,100,100);
+}
+      if (this.backgroundstate=== handy){
+  image(handy,0,0,100,100);
+}
+      if (this.backgroundstate=== hausTante){
+  image(hausTante,0,0,100,100);
+}
+      if (this.backgroundstate=== kueche){
+  image(kueche,0,0,100,100);
+}
+      if (this.backgroundstate=== musikladen){
+  image(musikladen,0,0,100,100);
+}
+      if (this.backgroundstate=== oberfeld){
+  image(oberfeld,0,0,100,100);
+}
+     if (this.backgroundstate=== park){
+  image(park,0,0,100,100);
+}
+      if (this.backgroundstate=== picknick){
+  image(picknick,0,0,100,100);
+}
+      if (this.backgroundstate=== schule){
+  image(schule,0,0,100,100);
+}
+     if (this.backgroundstate===  tanteDraussen){
+  image(tanteDraussen,0,0,100,100);
+}
+     if (this.backgroundstate===  tanteDraussenBp){
+  image(tanteDraussenBp,0,0,100,100);
+}
+     if (this.backgroundstate===  tanteEssen){
+  image(tanteEssen,0,0,100,100);
+}
+      if (this.backgroundstate=== teller){
+  image(teller,0,0,100,100);
+}
+     if (this.backgroundstate===  wohnzimmer){
+  image(wohnzimmer,0,0,100,100);
+}
+*/
     pop();
   }
   hitTest() {
