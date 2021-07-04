e567a00c13b707e6aea66416794cf5163fef2e49
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-07-04 04:33:19 +0200

    Create Export.js

---
+++ Export.js
@@ -0,0 +1,15 @@
+export default class Export {
+  constructor(bP) {
+    this.bP = bP;
+    this.messageArray = [
+      "1",
+      "2",
+      "3",
+      "4444 444444444 4444444 Ich bin " +
+        this.bP +
+        ", oder so.... Also zumindest vielleicht",
+      "5 hier ist noch ein langer Text bittesehr. 5 ist die Liebingszahl meiner Schwester",
+      "6",
+    ];
+  }
+}
