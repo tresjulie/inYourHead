507726f9d23cd3d842bbebde159a4240a2a6705e
tresjulie <julie.krabbe@stud.h-da.de>
2021-07-06 16:36:25 +0200

    instant Feedback aktuell

---
+++ export_instantFeedback.js
@@ -0,0 +1,13 @@
+export default class ExportInstantFeedback{
+    constructor(){
+    this.feedbackTextForNotebook = [];
+    this.instantFeedbackForNotebook = ["Ich bin der erste lange Text im Notebook passend zum ersten Instant Feedback",
+     "hallo ich bin Nummer 2 und ich bin auch dabei",
+     "test2",
+     "test 30000000000000",
+     "Gut so! Du musst dem bP genau das Vertrauen gegenüberbrigen das er und auch du jetzt gerade braucht"];
+
+    this.instantFeedback=["Ich bin das erste Instant Feedback, ganz klein und ganz fein. rund und schön.Passend für deinen Mund und deine Seele. Ich hpffe du bleibst gesund und munter und wir sehen uns bald wieder.hiii  hhoooo laooooo hasudhns kuhduvk hskhsdk hd kdhuk", 
+    "hallo ich bin die kleine Nummer 2"];
+    }
+}
--- julies_head.js
+++ julies_head.js
@@ -2,11 +2,14 @@ import NotebookIcon from "./notebookIcon.js";
 import InstantFeedback from "./instant_feedback.js";
 import Button from "./Button.js";
 import Notebook from "./instant_feedback.js";
+import ExportInstantFeedback from "./export_instantFeedback";
 
-let icon = new NotebookIcon(50, 50, 50, 50);
-let instantFeedback = new InstantFeedback(300, 50, 200, 100, "hallo");
+let icon = new NotebookIcon(35, 35, 50, 50);
+
 let entscheidungsButton = new Button(350, 300, "Entscheidung");
-let notebookPaper= new Notebook(50,50,200,200);
+let notebookPaper= new Notebook(35,35,477,200);
+let feedbackarray= new ExportInstantFeedback();
+let instantFeedback = new InstantFeedback(feedbackarray.instantFeedback[0]);
 
 /*hier könnten wir einfach eine Art Backlog anlegen 
 in dem wird die verschiedenen Instant Feedback Antworten 
@@ -16,60 +19,78 @@ welche Variable dann geunshiftet wird..?*/
 
 
 
-////
+
+
+
 let feedbackAnzeigen = false;
 let notebookAnzeigen=false;
 let n="";
 function draw() {
   clear();
   icon.display();
+  
+  if (feedbackAnzeigen === true) {
+    instantFeedback.messageHeight(instantFeedback.textLeading);
+    instantFeedback.display();    
+    instantFeedback.displayText();
+    feedbackarray.feedbackTextForNotebook.unshift(feedbackarray.instantFeedbackForNotebook[0]);
+    console.log(feedbackarray.feedbackTextForNotebook[1]);
+  }
   if(notebookAnzeigen===true){
     notebookPaper.displayNotebook();
   }
- /*hier müssen dann die richtigen If 
-  Bedingungen für die richtigen Situationen 
-  geschrieben werden, in denen dann n 
-  verändert werden kann*/
-  if (feedbackAnzeigen === true) {
-    instantFeedback.display(0);
-  }
-
-  if (feedbackAnzeigen === true) {
-    instantFeedback.display(1);
-  }
-
-  instantFeedback.feedbackTextForNotebook.push(instantFeedback.instantFeedbackForNotebook[0]);
-
-  instantFeedback.feedbackTextForNotebook.push(instantFeedback.instantFeedbackForNotebook[1]);
-
-  if(notebookAnzeigen===true){
-  for(let i=0; i<instantFeedback.feedbackTextForNotebook.length; i++){
-  // notebookPaper.displayNotebook(instantFeedback.feedbackTextForNotebook[i]);
-  fill(0);
-    text(instantFeedback.feedbackTextForNotebook[i], notebookPaper.x + 20, notebookPaper.y + 10 *i,instantFeedback.width -20);
-  }
-  }
 
 
-  instantFeedback.hitTest();
+  
+
+  // if(notebookAnzeigen===true){
+  // for(let i=0; i<feedbackarray.instantFeedback.length; i++){
+  //   fill(0);
+  //   text(feedbackarray.feedbackTextForNotebook[i], notebookPaper.x, 60+ notebookPaper.y*i,instantFeedback.width -20);
+  // } 
+  // // for (let i=0; i<feedbackarray.feedbackTextForNotebook.length; i++){}
+  // //     //hier sollten die Notebook sätze nach einander auftauchen 
+  // //  
+  // }
+
+  
+
+
+
+  
+
+  // instantFeedback.feedbackTextForNotebook.push(instantFeedback.instantFeedbackForNotebook[0]);
+
+  // instantFeedback.feedbackTextForNotebook.push(instantFeedback.instantFeedbackForNotebook[1]);
+
   entscheidungsButton.display();
 }
 
 function mouseClicked() {
+  // instantFeedback.hitTest();
   if (icon.hitTest()) {
     console.log("Icon geklickt");
     notebookAnzeigen=true;
 
   }
 
+
   if (entscheidungsButton.hitTest()) {
     console.log("Entscheidung getroffen");
     feedbackAnzeigen = true;
   }
+
   if (instantFeedback.hitTest()) {
     console.log("dreieck nicht ganz getroffen, aber soll in Zukunft so sein - feedback verschwindet");
     // instantFeedback.feedbackgelesen = false; //kann der das
-    feedbackAnzeigen = false;
+    // feedbackAnzeigen = false;
   }
 }
+
+
+
+
+
+
+
 
