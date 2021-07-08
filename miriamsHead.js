278fe24b18e0c76b5e334d58524ecefff4e9044a
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-07-08 11:28:17 +0200

    Update miriamsHead.js

--- miriamsHead.js
+++ miriamsHead.js
@@ -3,9 +3,8 @@ import PersonalBubble from "./PersonalBubble.js";
 import AnaPersonParameter from "./AnaPersonParameter.js";
 import WellBeing from "./WellBeing.js";
 import Export from "./Export.js";
-//import ScreenOrganisation from "./ScreenOrganisation.js";
-//let screens = new ScreenOrganisation(0, "pregame");
-
+import ScreenOrganisation from "./ScreenOrganisation.js";
+let screens = new ScreenOrganisation();
 let bPbubble = new PersonalBubble("#e2efde", "right");
 let otherbubble = new PersonalBubble("#ffcccc", "left");
 //bP
@@ -16,79 +15,59 @@ let pbNameArray = [];
 let bPName = " du ";
 let nameWritten = false;
 //bubble
-let minCounter = 1;
-let counter = 1;
-let maxCounter = 5;
+let minCounter = screens.min.speechBubble;
+let counter = screens.min.speechBubble;
+let maxCounter = screens.max.speechBubble;
 //in bubbleArray werden alle Objekte definiert
 let bubbleArray = [];
 let y = 300;
-let hitHeight = "";
 //in person und messageArray müssen an gleicher Stelle im Array die Inhalte (also wer sagt was) definiert werden
 let person = ["bP", "user", "user", "bP", "user", "user"];
-let array = new Export();
-
 function keyPressed() {
-
-  if (keyIsDown(8)) { // löschen
+  pbNameArray.push(key);
+  if (keyIsDown(8)) {
     pbNameArray.pop();
-    // pbNameArray.pop();
+    pbNameArray.pop();
   }
 
-  // if (keyIsDown(32)) { // Leertaste
-  //   // pbNameArray.pop();
-  //   pbNameArray.push(" ");
-  // }
-  fill(0, 255, 0);
-}
+  if (keyIsDown(32)) {
+    pbNameArray.pop();
+    pbNameArray.push(" ");
+  }
 
-//elemente mit Name neu definieren array[x]=yy
-function keyTyped(){
-  pbNameArray.push(key);  
-  if (keyIsDown(13)) { //enter
+  if (keyIsDown(13)) {
     pbNameArray.pop();
     nameWritten = true;
   }
+  fill(0, 255, 0);
 }
-
-/*
-let messageArray = [
-  "1",
-  "2",
-  "3",
-  "4444 444444444 " +
-    bPName +
-    " 4444444 Ich bin Jhin, oder so.... Also zumindest vielleicht",
-  "5 hier ist noch ein langer Text bittesehr. 5 ist die Liebingszahl meiner Schwester",
-  "6",
-];*/
 
 //Hilfe von Herr Toepper und https://p5js.org/examples/objects-array-of-objects.html
 //die Anzahl an Nachrichten definiert, wieviele neue Sprechblasen erzeugt wird
 
-/*function preload() {
-  let abendessen = loadImage("backgroundScreens/abendessen.png");
-  let baeckerei = loadImage("backgroundScreens/baeckerei.png");
-  let bekannte = loadImage("backgroundScreens/bekannte.png");
-  let bekannteTuer = loadImage("backgroundScreens/bekannteTuer.png");
-  let blumenladen = loadImage("backgroundScreens/blumenladen.png");
-  let entensee = loadImage("backgroundScreens/entensee.png");
-  let haeuser = loadImage("backgroundScreens/haeuser.png");
-  let handy = loadImage("backgroundScreens/handy.png");
-  let hausTante = loadImage("backgroundScreens/hausTante.png");
-  let kueche = loadImage("backgroundScreens/kueche.png");
-  let musikladen = loadImage("backgroundScreens/musikLaden.png");
-  let oberfeld = loadImage("backgroundScreens/oberfeld.png");
-  let park = loadImage("backgroundScreens/park.png");
-  let picknick = loadImage("backgroundScreens/picknick.png");
-  let schule = loadImage("backgroundScreens/schule.png");
-  let tanteDraussen = loadImage("backgroundScreens/tanteDraussen.png");
-  let tanteDraussenBp = loadImage("backgroundScreens/tanteDraussenBp.png");
-  let tanteEssen = loadImage("backgroundScreens/tanteEssen.png");
-  let teller = loadImage("backgroundScreens/teller.png");
-  let wohnzimmer = loadImage("backgroundScreens/wohnzimmer.png");
-  let tryIt = loadImage("try.png");
-}*/
-
+function preload() {
+  abendessen = loadImage("backgroundScreens/abendessen.png");
+  baeckerei = loadImage("backgroundScreens/baeckerei.png");
+  bekannte = loadImage("backgroundScreens/bekannte.png");
+  bekannteTuer = loadImage("backgroundScreens/bekannteTuer.png");
+  blumenladen = loadImage("backgroundScreens/blumenladen.png");
+  entensee = loadImage("backgroundScreens/entensee.png");
+  haeuser = loadImage("backgroundScreens/haeuser.png");
+  handy = loadImage("backgroundScreens/handy.png");
+  hausTante = loadImage("backgroundScreens/hausTante.png");
+  kueche = loadImage("backgroundScreens/kueche.png");
+  musikladen = loadImage("backgroundScreens/musikLaden.png");
+  oberfeld = loadImage("backgroundScreens/oberfeld.png");
+  park = loadImage("backgroundScreens/park.png");
+  picknick = loadImage("backgroundScreens/picknick.png");
+  schule = loadImage("backgroundScreens/schule.png");
+  tanteDraussen = loadImage("backgroundScreens/tanteDraussen.png");
+  tanteDraussenBp = loadImage("backgroundScreens/tanteDraussenBp.png");
+  tanteEssen = loadImage("backgroundScreens/tanteEssen.png");
+  teller = loadImage("backgroundScreens/teller.png");
+  wohnzimmer = loadImage("backgroundScreens/wohnzimmer.png");
+  tryIt = loadImage("try.png");
+}
 function mousePressed() {
   if (nameWritten === true) {
     if (counter < maxCounter) {
@@ -116,53 +95,57 @@ function mousePressed() {
 
 function draw() {
   background(255);
-  console.log(nameWritten);
-
+  screens.state.game = "ingame";
+  screens.backgroundNumber = 3;
   if (nameWritten === true) {
     bPName = pbNameArray.join("");
-    let array = new Export(bPName);
-    for (let i = 0; i < array.messageArray.length; i++) {
+    screens.display(
+      abendessen,
+      baeckerei,
+      bekannte,
+      bekannteTuer,
+      blumenladen,
+      entensee,
+      haeuser,
+      handy,
+      hausTante,
+      kueche,
+      musikladen,
+      oberfeld,
+      park,
+      picknick,
+      schule,
+      tanteDraussen,
+      tanteDraussenBp,
+      tanteEssen,
+      teller,
+      wohnzimmer,
+      startscreen,
+      tutorial1,
+      position,
+      nameIt
+    );
+    let expo = new Export(bPName);
+    for (let i = 0; i < expo.messageArray.length; i++) {
       if (i < maxCounter) {
         bubbleArray.push(
           new SpeechBubble(
             y,
-            array.messageArray[i],
+            expo.messageArray[i],
             person[i],
-            array.messageArray[i + 1]
+            expo.messageArray[i + 1]
           )
         );
       }
       if (i === maxCounter - 1) {
         bubbleArray.push(
-          new SpeechBubble(y, array.messageArray[i], person[i], ". .")
+          new SpeechBubble(y, expo.messageArray[i], person[i], ". .")
         );
       }
     }
 
     //screens
-    /*screens.display(
-    abendessen,
-    baeckerei,
-    bekannte,
-    bekannteTuer,
-    blumenladen,
-    entensee,
-    haeuser,
-    handy,
-    hausTante,
-    kueche,
-    musikladen,
-    oberfeld,
-    park,
-    picknick,
-    schule,
-    tanteDraussen,
-    tanteDraussenBp,
-    tanteEssen,
-    teller,
-    wohnzimmer,
-    tryIt
-  );*/
+
     //speech bubble
     if (counter < maxCounter) {
       bubbleArray[counter].y =
@@ -172,7 +155,7 @@ function draw() {
     }
 
     //counter geht jede Stelle der Array durch. Eine for-Schleife würde dabei alle Objekte gleichzeitig aufrufen, weshalb es so gelöst werden musste
-    if (counter < maxCounter) {
+    if (counter < maxCounter && screens.visible.speechBubble === true) {
       //die einzelnen Elemente von bubbleArray sind die Objekte der Klasse Speechbubble, die vor der function draw erzeugt wurden
       //es muss für jede Person, die Auftreten kann eine If-funktion erstellt werden. Die Anzahl ist aber überschaubar.
       if (bubbleArray[counter].person === "bP") {
@@ -187,7 +170,11 @@ function draw() {
 
     //durch diese if-Bedingung wird die letzte Sprechblase auch angezeigt
     //Sprechblasen gehen langsam nach oben
-    if (counter < maxCounter && counter - 1 >= minCounter) {
+    if (
+      counter < maxCounter &&
+      counter - 1 >= minCounter &&
+      screens.visible.speechBubble === true
+    ) {
       if (bubbleArray[counter - 1].person === "bP") {
         bubbleArray[counter - 1].all(bPbubble.direction, bPbubble.color);
       } else if (bubbleArray[counter - 1].person === "user") {
