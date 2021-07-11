7e530f7ca6130ce7b20039f6cdf867100bcf8071
tresjulie <julie.krabbe@stud.h-da.de>
2021-07-11 23:42:47 +0200

    Update type_name_screen.js

--- type_name_screen.js
+++ type_name_screen.js
@@ -1,5 +1,6 @@
 let nameBackground= loadImage("./Screens_ohneButtons/Name_2.jpg");
 let fieldPressed=false;
+let pbNameArray = [];
 
 let field ={
     x:467,
@@ -11,21 +12,7 @@ let field ={
 };
 
 
-function draw(){
- clear();
-//  keyPressed();
-//  keyTyped();
- image(nameBackground,0,0,1162,700);
 
- if(fieldPressed===true){
- push();
- noStroke();
- fill(field.r,field.g,field.b);
- rect(field.x,field.y,516,66,10);
- pop();
- }
-
-}
 
 function mouseClicked(){
 if(mouseX>field.x&& 
@@ -37,23 +24,43 @@ if(mouseX>field.x&&
 }
 
 
-// function keyPressed() {
-//     if (keyIsDown(8)) {
-//       //löschen
-//       pbNameArray.pop();
-//     }
-//     if (keyIsDown(32)) {
-//       //Leertaste
-//       pbNameArray.pop();
-//       pbNameArray.push(" ");
-//     }
-//   }
+function keyPressed() {
+    if (keyIsDown(8)) {
+      //löschen
+      pbNameArray.pop();
+    }
+    if (keyIsDown(32)) {
+      //Leertaste     
+      pbNameArray.push(" ");
+      pbNameArray.pop();
+ 
+    }
+}
 
 
-// function keyTyped(){
-//     pbNameArray.push(key);  
-//     if (keyIsDown(13)) { //enter
-//       pbNameArray.pop();
-//       nameWritten = true;
-//     }
-// }
+function keyTyped(){
+    pbNameArray.push(key);  
+    if (keyIsDown(13)) { //enter
+      pbNameArray.pop();
+      nameWritten = true;
+    } 
+} 
+
+function draw(){
+ clear();
+ 
+
+ image(nameBackground,0,0,1162,700);
+ if(fieldPressed===true){
+ push();
+ noStroke();
+ fill(field.r,field.g,field.b);
+ rect(field.x,field.y,516,66,10);
+ pop();
+ }      
+
+ textSize(30);
+ text(pbNameArray.join(""), field.x+20, field.y+45);
+ console.log(pbNameArray);
+
+}
