68eac9e1ad8ad001dde6c90cdb99ddb8b8e7f1dd
KirstenU <73792785+KirstenU@users.noreply.github.com>
2021-07-11 21:16:13 +0200

    Update TutorialScreen.js

--- TutorialScreen.js
+++ TutorialScreen.js
@@ -1,4 +1,4 @@
-import Button from "neueButtons.js";
+import Button from "./neueButtons.js";
 let verstandenbutton = new Button(825, 560, 230, 65, "Verstanden", "c9a6b9");
 let weiterbutton = new Button(825, 560, 230, 65, "Weiter", "c9a6b9");
 
